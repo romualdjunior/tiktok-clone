e5fc73118469072f1f6a5953ad155f7dc78a233f
romualdjunior <romuald.motchehokamguia@esprit.tn>
2023-03-13 21:47:05 +0100

    added utils

---
+++ utils/client.ts
@@ -0,0 +1,9 @@
+import sanityClient from '@sanity/client';
+
+export const client = sanityClient({
+  projectId: '2qjf6hld',
+  dataset: 'production',
+  apiVersion: '2023-03-10',
+  useCdn: false,
+  token: 'skq8JsCSLyG6eP1xCjn84VwHW6VBi8CfvxisdlOd7BNyW6r2lBjw8e4XrjRjTPRUCvmb3C8LOEQWddlT4FQSGBnjBJ4WfVE2fBpa5XkTkEVQvVwdC2KITZPreebM6wKyDi592VEghIFrEzyNSoYhj9HgPmxCI2aZoncudQ9QaYWOg7bJzeEX',
+});
